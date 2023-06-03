59055b13577617d4a8f97be7426aad4ded9c2012
Lovepreet Singh <lovepreetsingh@CREDBLRMAC1379-2.local>
2023-06-03 13:31:21 +0530

    Config added

---
+++ configuration/ragnarokConfiguration.go
@@ -0,0 +1,31 @@
+package configuration
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
+)
+
+var RagnarokConfig *Configuration
+
+type Configuration struct {
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+	} `yaml:"database"`
+}
+
+func configureRagnarok() error {
+	data, err := ioutil.ReadFile("../config.yaml")
+	if err != nil {
+		return err
+	}
+	RagnarokConfig := &Configuration{}
+	err = yaml.Unmarshal(data, RagnarokConfig)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/alphadecodex/nomise-ragnarok
 
 go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
